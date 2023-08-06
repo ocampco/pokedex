abda5f8e66ffa8b887fc030db5c8499525922ee1
ocampco <ocampco@gmail.com>
2023-08-06 15:51:07 +0700

    Display loading message when fetching

--- src/List.js
+++ src/List.js
@@ -4,6 +4,7 @@ import { LIST_URL } from './config';
 
 const List = () => {
   const [list, setList] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchList = async () => {
@@ -11,15 +12,18 @@ const List = () => {
       const json = await response.json();
       const results = transformList(json)
 
+      setIsLoading(false);
       setList(results);
     };
 
     fetchList();
   }, []);
 
+  if (isLoading) return <div>Loading...</div>
+
   return (
     <ul>
-      { list && list.map(({ name, url, id }) => (
+      { !isLoading && list.map(({ name, url, id }) => (
           <li key={id}>{id} - {name}</li>
       ))}
     </ul>
