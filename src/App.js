5c3546c0c2666c19259d64039a69f1df769f6db6
ocampco <ocampco@gmail.com>
2023-08-07 12:47:43 +0700

    Refactor to shorthand

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import Detail from './Detail';
 import Select from './Select';
 import styles from './App.module.css';
 
-const App = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          pokedex
-        </h1>
-          <Select />
-          <Detail />
-      </div>
+const App = () => (
+  <div className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>
+        pokedex
+      </h1>
+        <Select />
+        <Detail />
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
