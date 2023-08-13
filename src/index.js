b107f37b46b318b9ee2f554388ffe7471dfe5ab3
ocampco <ocampco@gmail.com>
2023-08-13 16:58:59 +0700

    Remove web vitals boilerplate

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import reportWebVitals from './reportWebVitals';
 import './styles/index.css';
 
 const root = createRoot(document.getElementById('root'));
@@ -12,8 +11,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
